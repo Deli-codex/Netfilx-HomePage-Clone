c050155fac74fd0b7a132d8d49ee2a050dfe8702
Deli-codex <omoleoladele43@gmail.com>
2023-10-04 15:37:50 +0100

    my first git clone as regards the clone seems to be no issues ye

---
+++ README.md
@@ -0,0 +1,3 @@
+/** this clone is to further Enhance my front end skills 
+/** Done in less than 2days
+ with HTML css and Vanilla Javascript 
---
+++ index.js
@@ -0,0 +1,6 @@
+
+const questionButton = document.querySelector('.QuestionOne')
+questionButton.addEventListener('click', () =>{
+    const reveal = document.querySelector('.ansOne')
+    reveal.style.display = 'block'; 
+})
